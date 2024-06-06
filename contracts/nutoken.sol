b7141945c24701117cbf1506a80527a17cbae515
pollyjohn <joaotv16@gmail.com>
2024-06-06 20:26:22 -0300

    Sol Smart Contracts activity

---
+++ contracts/nutoken.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract NubankSharesToken {
+    string public name = "Nubank Shares Token";
+    string public symbol = "NST";
+    uint8 public decimals = 18;
+    uint256 private _totalSupply;
+
+    address public deployer;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    modifier onlyDeployer() {
+        require(msg.sender == deployer, "Caller is not the deployer");
+        _;
+    }
+
+    constructor() {
+        deployer = msg.sender;
+    }
+
+    function mint(address account, uint256 amount) external onlyDeployer {
+        require(account != address(0), "Mint to the zero address");
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        require(recipient != address(0), "Transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "Transfer amount exceeds balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        require(sender != address(0), "Transfer from the zero address");
+        require(recipient != address(0), "Transfer to the zero address");
+        require(_balances[sender] >= amount, "Transfer amount exceeds balance");
+        require(_allowances[sender][msg.sender] >= amount, "Transfer amount exceeds allowance");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        _allowances[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+}
